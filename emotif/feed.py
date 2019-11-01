60fadd8db80065ffa98dc04753c7c865a02e8803
shakeeb1998 <shakeebsiddiqui1998@gmail.com>
2019-11-01 12:02:02 +0500

    test added

--- emotif/feed.py
+++ emotif/feed.py
@@ -121,38 +121,42 @@ def increase_resolution():
 
 
 @frappe.whitelist(allow_guest=True)
-def test(img_data):
+def test():
 
 
-    # def SuperRes(path):
-    #     model_path = '/home/frappe/frappe-bench/apps/emotif/emotif/ESRGAN/models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
-    #     print('device loading')
-    #     device = torch.device('cpu')  # if you want to run on CPU, change 'cuda' -> cpu
-    #     print('device loaded')
-    #     model = arch.RRDBNet(3, 3, 64, 23, gc=32)
-    #     model.load_state_dict(torch.load(model_path), strict=True)
-    #     model.eval()
-    #     model = model.to(device)
-    #
-    #     print('Model path {:s}. \nTesting...'.format(model_path))
-    #
-    #     img = cv2.imread(path, cv2.IMREAD_COLOR)
-    #     print(img.shape)
-    #     img = img * 1.0 / 255
-    #     img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
-    #     img_LR = img.unsqueeze(0)
-    #     img_LR = img_LR.to(device)
-    #
-    #     with torch.no_grad():
-    #         output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
-    #     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
-    #     output = (output * 255.0).round()
-    #     print("writing result")
-    #     cv2.imwrite('/home/frappe/frappe-bench/apps/emotif/emotif/ESRGAN/LR/result.png', output)
-    #     print(output.shape)
-    #     return output
-    #
-    # SuperRes('/home/frappe/frappe-bench/apps/emotif/emotif/ESRGAN/LR/baboon.png')
-    print(len(img_data))
+    def SuperRes(path):
+        model_path = '/home/frappe/frappe-bench/apps/emotif/emotif/ESRGAN/models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
+        print('device loading')
+        device = torch.device('cpu')  # if you want to run on CPU, change 'cuda' -> cpu
+        print('device loaded')
+        model = arch.RRDBNet(3, 3, 64, 23, gc=32)
+        model.load_state_dict(torch.load(model_path), strict=True)
+        model.eval()
+        model = model.to(device)
+
+        print('Model path {:s}. \nTesting...'.format(model_path))
+
+        img = cv2.imread(path, cv2.IMREAD_COLOR)
+        print(img.shape)
+        img = img * 1.0 / 255
+        img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+        img_LR = img.unsqueeze(0)
+        img_LR = img_LR.to(device)
+
+        with torch.no_grad():
+            output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+        output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
+        output = (output * 255.0).round()
+        print("writing result")
+        cv2.imwrite('/home/frappe/frappe-bench/apps/emotif/emotif/ESRGAN/LR/result.png', output)
+        print(output.shape)
+        return output
+
+    SuperRes('/home/frappe/frappe-bench/apps/emotif/emotif/ESRGAN/LR/baboon.png')
+    # print(len(img_data))
     print('shakeeb pot')
     return 'jhh'
+
+
+def test2():
+    return 'file updated'
